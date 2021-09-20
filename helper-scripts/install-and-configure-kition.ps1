ca42abc36811cc40c75a799ad05254454d702479
itsfridaythen <itsfridayagain@protonmail.ch>
2021-09-20 18:05:31 +0300

    update version for helper script

--- helper-scripts/install-and-configure-kition.ps1
+++ helper-scripts/install-and-configure-kition.ps1
@@ -2,16 +2,16 @@ setx VENIDIUM_ROOT "$env:HOMEDRIVE$env:HOMEPATH\.venidium\kition"
 
 Write-Output "`n`nPlease wait. The installation will take some time`n`n"
 
-Start-Process -WorkingDirectory (Get-Item .).FullName -FilePath "VenidiumSetup-0.3.65.exe" -ArgumentList "--silent" -Wait
+Start-Process -WorkingDirectory (Get-Item .).FullName -FilePath "VenidiumSetup-0.3.73.exe" -ArgumentList "--silent" -Wait
 
 Write-Output "`n`nPlease wait. Kition testnet is now being configured`n`n"
 
-& "$env:localappdata\venidium-blockchain\app-0.3.65\resources\app.asar.unpacked\daemon\Venidium.exe" init
-& "$env:localappdata\venidium-blockchain\app-0.3.65\resources\app.asar.unpacked\daemon\Venidium.exe" keys generate
-& "$env:localappdata\venidium-blockchain\app-0.3.65\resources\app.asar.unpacked\daemon\Venidium.exe" configure --testnet true
+& "$env:localappdata\venidium-blockchain\app-0.3.73\resources\app.asar.unpacked\daemon\Venidium.exe" init
+& "$env:localappdata\venidium-blockchain\app-0.3.73\resources\app.asar.unpacked\daemon\Venidium.exe" keys generate
+& "$env:localappdata\venidium-blockchain\app-0.3.73\resources\app.asar.unpacked\daemon\Venidium.exe" configure --testnet true
 
 Write-Output "`n`nLaunching Venidium Blockchain`n`n"
 
-Start-Process -WorkingDirectory "$env:localappdata\venidium-blockchain\app-0.3.65" -FilePath "$env:localappdata\venidium-blockchain\Venidium.exe"
+Start-Process -WorkingDirectory "$env:localappdata\venidium-blockchain\app-0.3.73" -FilePath "$env:localappdata\venidium-blockchain\Venidium.exe"
 
 [Environment]::Exit(0)
