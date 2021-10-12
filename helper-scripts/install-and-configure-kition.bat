6e963337be42c30d3486d38f92789e09da8d7f58
itsfridaythen <itsfridayagain@protonmail.ch>
2021-10-12 11:17:18 +0300

    update helper script

--- helper-scripts/install-and-configure-kition.bat
+++ helper-scripts/install-and-configure-kition.bat
@@ -17,23 +17,23 @@ $env:VENIDIUM_ROOT = "$env:HOMEDRIVE$env:HOMEPATH\.venidium\kition"
 
 Write-Output "`n`nPlease wait. The installation will take some time`n`n"
 
-Start-Process -WorkingDirectory (Get-Item .).FullName -FilePath "VenidiumSetup-0.4.1.exe" -ArgumentList "--silent" -Wait
+Start-Process -WorkingDirectory (Get-Item .).FullName -FilePath "VenidiumSetup-0.4.3.exe" -ArgumentList "--silent" -Wait
 
 Write-Output "`n`nPlease wait. Kition testnet is now being configured`n`n"
 
-& "$env:localappdata\venidium-blockchain\app-0.4.1\resources\app.asar.unpacked\daemon\Venidium.exe" init
+& "$env:localappdata\venidium-blockchain\app-0.4.3\resources\app.asar.unpacked\daemon\Venidium.exe" init
 
-$keys = & "$env:localappdata\venidium-blockchain\app-0.4.1\resources\app.asar.unpacked\daemon\Venidium.exe" keys show
+$keys = & "$env:localappdata\venidium-blockchain\app-0.4.3\resources\app.asar.unpacked\daemon\Venidium.exe" keys show
 
 if ( $keys -eq "There are no saved private keys" )
 {
-    & "$env:localappdata\venidium-blockchain\app-0.4.1\resources\app.asar.unpacked\daemon\Venidium.exe" keys generate
+    & "$env:localappdata\venidium-blockchain\app-0.4.3\resources\app.asar.unpacked\daemon\Venidium.exe" keys generate
 }
 
-& "$env:localappdata\venidium-blockchain\app-0.4.1\resources\app.asar.unpacked\daemon\Venidium.exe" configure --testnet true
+& "$env:localappdata\venidium-blockchain\app-0.4.3\resources\app.asar.unpacked\daemon\Venidium.exe" configure --testnet true
 
 Write-Output "`n`nLaunching Venidium Blockchain`n`n"
 
-Start-Process -WorkingDirectory "$env:localappdata\venidium-blockchain\app-0.4.1" -FilePath "$env:localappdata\venidium-blockchain\Venidium.exe"
+Start-Process -WorkingDirectory "$env:localappdata\venidium-blockchain\app-0.4.3" -FilePath "$env:localappdata\venidium-blockchain\Venidium.exe"
 
 [Environment]::Exit(0)
