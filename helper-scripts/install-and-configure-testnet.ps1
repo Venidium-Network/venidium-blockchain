176e0978cfd19b7d4ce29ccf3059830a915003bd
itsfridaythen <itsfridayagain@protonmail.ch>
2021-10-21 10:07:43 +0300

    replace testnet configuration script with a powershell script

---
+++ helper-scripts/install-and-configure-testnet.ps1
@@ -0,0 +1,28 @@
+Write-Output "setting up env vars"
+$VENIDIUM_ROOT = "$HOMEDRIVE$HOMEPATH\.venidium\kition\"
+[Environment]::SetEnvironmentVariable('VENIDIUM_ROOT', $VENIDIUM_ROOT, 'User')
+
+Write-Output "Please wait. The installation will take some time"
+
+Start-Process -WorkingDirectory (Get-Item .).FullName -FilePath "VenidiumSetup-1.0.0.exe" -ArgumentList "--silent" -Wait
+
+Write-Output "Please wait. Kition testnet is now being configured"
+
+& "$env:localappdata\venidium-blockchain\app-1.0.0\resources\app.asar.unpacked\daemon\Venidium.exe" init
+
+$KEYS = & "$env:localappdata\venidium-blockchain\app-1.0.0\resources\app.asar.unpacked\daemon\Venidium.exe" keys show
+
+if ( "$KEYS" -eq 'There are no saved private keys' )
+{
+    Write-Output "There are no keys"
+    Write-Output "Generating new key"
+    & "$env:localappdata\venidium-blockchain\app-1.0.0\resources\app.asar.unpacked\daemon\Venidium.exe" keys generate
+}
+
+& "$env:localappdata\venidium-blockchain\app-1.0.0\resources\app.asar.unpacked\daemon\Venidium.exe" configure --testnet true
+
+Write-Output "Launching Venidium Blockchain"
+
+Start-Process -WorkingDirectory "$env:localappdata\venidium-blockchain\app-1.0.0" -FilePath "$env:localappdata\venidium-blockchain\Venidium.exe"
+
+# [Environment]::Exit(0)
