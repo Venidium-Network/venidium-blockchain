c3fd0600e8571ba11d28a0bb022b1763fb1d7956
ItsFridayThen <itsfridayagain@protonmail.ch>
2021-11-03 01:51:28 +0200

    fix domain name

--- venidium/server/server.py
+++ venidium/server/server.py
@@ -714,7 +714,7 @@ class VenidiumServer:
         try:
             timeout = ClientTimeout(total=15)
             async with ClientSession(timeout=timeout) as session:
-                async with session.get("https://ip.venidium.net/") as resp:
+                async with session.get("https://ip.venidium.io/") as resp:
                     if resp.status == 200:
                         ip = str(await resp.text())
                         ip = ip.rstrip()
