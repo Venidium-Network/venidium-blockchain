3f3cdb72f05617e4b344186ee939d30eabc9b753
ItsFridayThen <itsfridayagain@protonmail.ch>
2021-11-03 01:38:59 +0200

    fix bundle id

--- build_scripts/build_macos_m1.sh
+++ build_scripts/build_macos_m1.sh
@@ -55,7 +55,7 @@ cp package.json package.json.orig
 jq --arg VER "$VENIDIUM_INSTALLER_VERSION" '.version=$VER' package.json > temp.json && mv temp.json package.json
 
 electron-packager . Venidium --asar.unpack="**/daemon/**" --platform=darwin \
---icon=src/assets/img/Venidium.icns --overwrite --app-bundle-id=net.venidium.blockchain \
+--icon=src/assets/img/Venidium.icns --overwrite --app-bundle-id=io.venidium.blockchain \
 --appVersion=$VENIDIUM_INSTALLER_VERSION
 LAST_EXIT_CODE=$?
 
@@ -109,7 +109,7 @@ fi
 #
 # Ask for username and password. password should be an app specific password.
 # Generate app specific password https://support.apple.com/en-us/HT204397
-# xcrun altool --notarize-app -f Venidium-0.1.X.dmg --primary-bundle-id net.venidium.blockchain -u username -p password
+# xcrun altool --notarize-app -f Venidium-0.1.X.dmg --primary-bundle-id io.venidium.blockchain -u username -p password
 # xcrun altool --notarize-app; -should return REQUEST-ID, use it in next command
 #
 # Wait until following command return a success message".
