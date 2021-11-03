1e74f218892b129bf53a6a910c33a9c8b92af034
ItsFridayThen <itsfridayagain@protonmail.ch>
2021-11-03 05:10:47 +0200

    xch-> xvm

--- venidium/cmds/units.py
+++ venidium/cmds/units.py
@@ -3,7 +3,7 @@ from typing import Dict
 # The rest of the codebase uses rays everywhere.
 # Only use these units for user facing interfaces.
 units: Dict[str, int] = {
-    "venidium": 10 ** 12,  # 1 venidium (XCH) is 1,000,000,000,000 ray (1 trillion)
+    "venidium": 10 ** 12,  # 1 venidium (XVM) is 1,000,000,000,000 ray (1 trillion)
     "ray": 1,
     "colouredcoin": 10 ** 3,  # 1 coloured coin is 1000 colouredcoin rays
 }
